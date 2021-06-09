15b1aab39b3c1a9eed237e3ebbd7c0d102d87c46
Yuna <yunahaj@gmail.com>
2021-06-10 00:34:07 +0900

    router 적용

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,10 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div v-for="item in articles" :key="item.id">
-        <img :src="getImageUrl(item.image)"
-             alt="" width="100" height="100">
-        <span class="title">{{ item.title }}</span>
-        <span>{{ item.content }}</span>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
-  import {db} from '../firebase'
-
   export default {
-    name: 'App',
-    components: {
-      HelloWorld
-    },
-    data() {
-      return {
-        articles: []
-      }
-    },
-    methods: {
-      getArticles() {
-        db.collection('articles').onSnapshot((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            this.articles.push({id: doc.id, ...doc.data()})
-          });
-        });
-      },
-      getImageUrl(image) {
-        return `https://firebasestorage.googleapis.com/v0/b/kinfork-d24c7.appspot.com/o/${image}?alt=media`
-      }
-    },
-    created() {
-      this.getArticles()
-    }
+    name: 'App'
   }
 </script>
 
@@ -49,9 +16,5 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
-    }
-
-    .title {
-        margin-right: 5px;
     }
 </style>
